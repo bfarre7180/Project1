373c9afc5a1f674ca7f0b8efd27b26ebe4f5bf80
bfarrel9 <bfarrel9@general3.opsprod.asu.edu>
2025-03-07 03:49:20 -0700

    V2

--- hash.cpp
+++ hash.cpp
@@ -9,12 +9,17 @@ using namespace std;
 int hash_function(string text) {
     // Implement your own hash function here
     int hash = 1;
-    int multiplier = 3;
+    double multiplier = tan(text.size() * text.at((text.size() * (text.at(31 % text.size()) + text.at(23 % text.size()))) % text.size()));
+    int hash2 = 1;
 
     for(char c: text) {
-        hash = (hash * 31 * c) * multiplier;
+        hash = (int)((double)(hash * 31 * c) * multiplier);
         multiplier += c;
+        hash += (int)tan(multiplier);
+        hash2 = (INT_MAX ^ hash ^ hash2) << 2;
     }
+
+    hash ^= hash2;
 
     return (hash >= 0) ? hash: -1 * hash;
 }
